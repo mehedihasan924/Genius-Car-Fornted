a67363aefd9825eeb550c1f8f7cb54d863c56e5f
mehedihasan924 <mdhasanmehedi924@gmail.com>
2024-03-15 01:06:38 +0600

    added

--- src/Component/Pages/Home/Banner/Banner.jsx
+++ src/Component/Pages/Home/Banner/Banner.jsx
@@ -4,13 +4,58 @@ import img2 from '../../../../assets/images/banner/2.jpg';
 import img3 from '../../../../assets/images/banner/3.jpg';
 import img4 from '../../../../assets/images/banner/4.jpg';
 import img5 from '../../../../assets/images/banner/5.jpg';
+import './Banner.css'
+import BannerItems from './BannerItems';
+
+const bannerData = [
+    {
+      image: img1,
+      prev: 5,
+      id: 1,
+      next:2
+    },
+     {
+      image: img2,
+      prev: 1,
+      id: 2,
+      next:3
+    },
+       {
+      image: img3,
+      prev: 2,
+      id: 3,
+      next:4
+    },
+        {
+      image: img4,
+      prev: 3,
+      id: 4,
+      next:5
+    },
+    {
+      image: img5,
+      prev: 4,
+      id: 5,
+      next:1
+     }
+   ]
+
 const Banner = () => {
-   
   return (
-      <div>
-          <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src={img1} className="w-full" />
+  <div>
+      <div className="carousel w-full">
+        {
+          bannerData.map(slider =>
+            <BannerItems
+              key={slider.id}
+              slider={slider}
+            >     
+          </BannerItems>)
+        }
+ {/* <div id="slide2" className="carousel-item relative w-full">
+    <div className='carousel-img'>
+      <img src={img2} className="w-full" />
+    </div>
     <div className="absolute flex justify-end gap-2 transform -translate-y-1/2  left-24 
       top-1/4 ">
         <h1 className='text-6xl font-bold text-white'>
@@ -30,35 +75,8 @@ const Banner = () => {
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src={img2} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={img3} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src={img4} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
- <div id="slide4" className="carousel-item relative w-full">
-    <img src={img5} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
+  </div>  */}
+
 </div>
      </div>
   )
---
+++ src/Component/Pages/Home/Banner/BannerItems.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+const BannerItems = ({slider}) => {
+    const { image , id, prev, next} = slider;
+  return (
+   <div id={`slide${id}`} className="carousel-item relative w-full">
+    <div className='carousel-img'>
+      <img src={image} className="w-full" />
+    </div>
+    <div className="absolute flex justify-end gap-2 transform -translate-y-1/2  left-24 
+      top-1/4 ">
+        <h1 className='text-6xl font-bold text-white'>
+            Affordable <br />
+            Price for Car <br />
+            Servicing
+        </h1>
+    </div>
+    <div className="absolute flex justify-end gap-2 transform -translate-y-1/2 w-2/5 left-24  top-1/2">
+            <p className='text-white text-xl'>There are many variations of passages of available,But the Majority Have suffered Alterration in Some Form </p>
+                  </div>
+       <div className="absolute flex justify-start gap-2 transform -translate-y-1/2 w-2/5 left-24  top-3/4">
+            <button className="btn btn-active btn-primary">Primary</button>
+            <button className="btn btn-outline btn-secondary">Secondary</button>
+     </div>               
+    <div className="absolute flex justify-end gap-2 transform -translate-y-1/2 left-24  right-5 bottom-0">
+      <a href={`#slide${prev}`} className="btn btn-circle">❮</a> 
+      <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  )
+}
+
+export default BannerItems
